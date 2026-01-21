deceb03c69d332b103b998b8577c112f4d18220e
Varnwal Vivek <varnwalvivek4644@gmail.com>
2026-01-21 14:53:43 +0530

    ..

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,8 +1,6 @@
 const Usermodel = require("../model/UserModel");
 
 
-
-
 const getapi = async (req, res) => {
   try {
     console.log("Hello World");
@@ -20,24 +18,24 @@ const getapi = async (req, res) => {
   }
 };
 
-const addUser = async (req, res) => {
+// const addUser = async (req, res) => {
 
-    console.log("User added",req.body);
-  try {
-    console.log("User added",req.body);
-    const user = await Usermodel.create(req.body);
+//     console.log("User added",req.body);
+//   try {
+//     console.log("User added",req.body);
+//     const user = await Usermodel.create(req.body);
 
-    res.status(201).json({
-      message: "User added successfully",
-      data: user
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: "Error adding user",
-      error: error.message
-    });
-  }
-};
+//     res.status(201).json({
+//       message: "User added successfully",
+//       data: user
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       message: "Error adding user",
+//       error: error.message
+//     });
+//   }
+// };
 
 
 
--- router/UserRouter.js
+++ router/UserRouter.js
@@ -4,6 +4,6 @@ const router = require("express").Router();
 
 const UserController = require("../controller/UserController");
 router.get("/getapi",UserController.getapi);
-router.post("/adduser",UserController.addUser);
+// router.post("/adduser",UserController.addUser);
 
 module.exports = router;
